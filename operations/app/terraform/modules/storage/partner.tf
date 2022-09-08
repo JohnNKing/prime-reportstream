6746fe9454539b70f8e111f7620a98c656e31d75
lucasdze <lucasdze@gmail.com>
2022-09-08 13:41:08 -0500

    checkov exception list

--- operations/app/terraform/modules/storage/partner.tf
+++ operations/app/terraform/modules/storage/partner.tf
@@ -6,4 +6,34 @@ resource "azurerm_storage_container" "storage_container_hhsprotect" {
 resource "azurerm_storage_container" "storage_container_dcipher" {
   name                 = "dcipher"
   storage_account_name = azurerm_storage_account.storage_partner.name
+  queue_properties {
+    logging {
+      read = true
+    }
+  }
 }
+
+
+
+
+# queue_properties  {
+#     logging {
+#         delete                = true
+#         read                  = true
+#         write                 = true
+#         version               = "1.0"
+#         retention_policy_days = 10
+#     }
+#     hour_metrics {
+#         enabled               = true
+#         include_apis          = true
+#         version               = "1.0"
+#         retention_policy_days = 10
+#     }
+#     minute_metrics {
+#         enabled               = true
+#         include_apis          = true
+#         version               = "1.0"
+#         retention_policy_days = 10
+#     }
+#   }
